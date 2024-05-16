10b8bf93458c621f988403903635f2e7a17f5346
Matheus Ritzmann <123838378+Hiroshi1401@users.noreply.github.com>
2024-05-15 22:20:06 -0300

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-function escolhaGuardaChuva() {
-    // Redirecionar para o link do vídeo do Twitter
-    window.location.href = "https://x.com/PostsOfCats/status/1788139661581279427"; 
-}
-
-function escolhaCorrer() {
-    document.getElementById('feedback').innerHTML = "Desculpa, escolha errada, tente novamente :)";
-}
